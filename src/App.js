fab6674d7854d5eb756f3706b564aa797bf15396
daniel <dannyvazquez@gmail.com>
2022-06-16 18:38:18 -0600

    apagador listo

--- src/App.js
+++ src/App.js
@@ -7,14 +7,8 @@ import Title from "./components/Title";
 import FullName from "./components/FullName";
 
 function App() {
-  // Local state
-  // [0] = valor del state
-  // [1] = función que actualiza el state
-  // si `useState` recibe un argumento, este será el valor inicial del estado
-  // si no el estado es undefined
   const [count, setCount] = useState(0);
-  console.log(count, "COUNT");
-  console.log(setCount, "SET_COUNT");
+  const [isOn, setIsOn] = useState(false);
 
   const handleClick = (increase) => {
     let newCount = count;
@@ -26,15 +20,30 @@ function App() {
     setCount(newCount);
   };
 
+  const isMultiple = count % 5 === 0 && count !== 0 ? true : false;
+  const countType = isMultiple ? "Es multiplo de 5" : "No es multiplo de 5";
+  const countClass = isMultiple ? "active" : "inactivo";
+
+  const squareClass = isOn ? "square-on" : "square-off";
+  const squareText = isOn ? "ON" : "OFF";
   return (
     <div className="App">
       <header className="App-header">
         <Title text="Aaron"></Title>
         <FullName firstName="Luis" lastName="Vera"></FullName>
-
-        <p>Counter: {count}</p>
+        <p className={countClass}>
+          Counter: {count}, {countType}
+        </p>
         <button onClick={() => handleClick(true)}>Increase</button>
-        <button onClick={() => handleClick(false)}>Decrease</button>
+
+        <div className={`square ${squareClass}`}>{squareText}</div>
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <button onClick={() => setIsOn(!isOn)}>Interruptor</button>
+        </div>
       </header>
     </div>
   );
