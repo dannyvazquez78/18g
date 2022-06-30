2fd95e8f621721aef85d914c4485d3afc921321c
daniel <dannyvazquez@gmail.com>
2022-06-29 19:20:30 -0600

    completo el crud

--- src/containers/ProfileContainer/index.jsx
+++ src/containers/ProfileContainer/index.jsx
@@ -6,11 +6,11 @@ import "./ProfileContainer.css";
 export default function ProfileContainer() {
   return (
     <div className="Profile-container">
-      <h1>Profile</h1>
+      <h1>Profiles</h1>
       <div className="Profile-nav">
         <nav>
-          <Link to="">Detalle</Link>
-          <Link to="edit">Edit</Link>
+          <Link to="create">Create</Link>
+          <Link to="Edit">Edit</Link>
         </nav>
       </div>
       <Outlet />
--- src/pages/Profile/Detail/index.jsx
+++ src/pages/Profile/Detail/index.jsx
@@ -1,3 +1,53 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { retrieve as retrieveUser } from "../../../services/users";
+
+// CSS
+import "./Detail.css";
+
 export default function ProfileDetail() {
-    return <h2>Detail</h2>;
-  }
+  // Local state
+  const [user, setUser] = useState(null);
+
+  // RRD
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getUser = async () => {
+      const data = await retrieveUser(id);
+      setUser(data);
+    };
+
+    getUser();
+  }, [id]);
+
+  console.log(id);
+
+  return (
+    <div>
+      <h2>Detail</h2>
+      {!user ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <img alt="profile" src={user.photoURL} />
+          <p>
+            {user.firstName} {user.lastName}
+          </p>
+          <p>{user.email}</p>
+          <p>{user.birthdate}</p>
+          <button onClick={() => navigate(-1)}>Regresar</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+// EJERCICIO: EDIT PROFILE
+// 1. Recibir variable de la URL
+// 2. Consultar la información en la DB (firebase)
+// 3. Cuando se tenga disponible la información,
+// ACTUALIZAR EL ESTADO DE LOS INPUTS
+// 4. Crear formulario
+// 5. Enviar PATCH a firebase
